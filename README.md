d9cb9d40986926965e21de43c2fb24f74c305b30
emtee14 <48140121+emtee14@users.noreply.github.com>
2021-03-19 22:06:03 +0000

    Boilerplate Code

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Backend
+# Backend for Store Med
+
+## Setup
+### Dependencies
+```bash
+pip install -r requirements.txt
+```
+### Enviroment Variable
+Change the name of example.env to .env and replace apropriate variable with their values.
+```
+SECRET_KEY=<something-random>
+MONGODB_USERNAME=<username-for-DB>
+MONGODB_PASSWORD=<password-for-DB>
+MONGODB_HOST=<DB-domain>
+```
+
+# Local Deployment
+## Windows
+### Powershell
+```powershell
+$env:FLASK_APP = "app.app"
+flask run
+```
+### CMD
+```cmd
+set FLASK_APP=app.app
+flask run
+```
+## Mac/Linux
+### Bash
+```bash
+export FLASK_APP=app.app
+flask run
+```
---
+++ app/main.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify
+from flask_jwt_extended import (JWTManager, create_access_token, get_jwt,
+                                get_jwt_identity, set_access_cookies)
+from decouple import config
+from datetime import datetime, timedelta, timezone
+
+app = Flask(__name__)
+
+## JWT settings and init
+app.config["JWT_COOKIE_SECURE"] = False
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+app.config['JWT_SECRET_KEY'] = config("SECRET_KEY")
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=3)
+app.config["SECRET_KEY"] = config("SECRET_KEY")
+jwt = JWTManager(app)
+
+## JWT refresher
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
+
+## Unauthorized request handler
+@jwt.unauthorized_loader
+def my_invalid_token_callback(expired_token):
+    return jsonify(success=False, error="Invalid token or user isn't logged in"), 403
+
+if __name__ == '__main__':
+    app.run(debug=True)
