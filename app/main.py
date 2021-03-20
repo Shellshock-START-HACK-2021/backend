d4b4aee92f02230608b9b43cf7c3a70fb7fc04ae
Morgan <48140121+emtee14@users.noreply.github.com>
2021-03-20 09:23:39 +0000

    changes to main.py

--- app/main.py
+++ app/main.py
@@ -10,8 +10,7 @@ from app import auth, user
 app = Flask(__name__)
 
 # Allow COR for whole app
-app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
-CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.register_blueprint(auth.bp, url_prefix="/auth")
 app.register_blueprint(user.bp, url_prefix="/user")
